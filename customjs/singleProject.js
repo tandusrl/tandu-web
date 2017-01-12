bb74abdbf0c8d3b862332e4cd6dcb9ef36aa3401
Sara <sarabertaina@hotmail.it>
2017-01-12 17:24:13 +0100

    Built jekyll-project from commit 380e9fe

--- customjs/singleProject.js
+++ customjs/singleProject.js
@@ -23,28 +23,30 @@ $(document).ready(function() {
       $($el).css('stroke-dashoffset', lineLength);
   }
 
-  $('#pc-project-svg path, #tablet-project-svg path, #phone-project-svg path, #arrow-svg path').each(function(index){
+  $('#empty-cloud path, #empty-app path, #phone-project-svg path, #arrow-svg path').each(function(index){
       pathPrepare(this);
   });
 
 /*----- Animation PC */
   var tween = new TimelineMax();
-      tween.add(TweenMax.to($('#pc-project-svg path'), 1, {strokeDashoffset: 0, ease:Linear.easeNone}));
+      tween.add(TweenMax.to($('#empty-cloud path'), 1, {strokeDashoffset: 0, ease:Linear.easeNone}));
 
   var controller = new ScrollMagic.Controller();
 
-  var scene0 = new ScrollMagic.Scene({triggerElement: '#tnd-single-project-spacer', duration: 300, tweenChanges: true})
+  var scene0 = new ScrollMagic.Scene({triggerElement: '#animation-start', duration: 300, tweenChanges: true})
       .setTween(tween)
       //.addIndicators()
+      .reverse(false)
       .addTo(controller);
 /*----- END Animation PC */
 
 /*---- Animation TABLET */
   var tweenTablet = new TimelineMax();
-      tweenTablet.add(TweenMax.to($('#tablet-project-svg  path'), 1, {strokeDashoffset: 0, ease:Linear.easeNone}));
+      tweenTablet.add(TweenMax.to($('#empty-app  path'), 1, {strokeDashoffset: 0, ease:Linear.easeNone}));
 
-  var scene0 = new ScrollMagic.Scene({triggerElement: '#tnd-single-project-spacer', duration: 300, tweenChanges: true})
+  var scene0 = new ScrollMagic.Scene({triggerElement: '#animation-start', duration: 300, tweenChanges: false})
       .setTween(tweenTablet)
+      .reverse(false)
       .addTo(controller);
 /*----- END Animation TABLET */
 
@@ -53,7 +55,7 @@ $(document).ready(function() {
   var tweetPhone = new TimelineMax();
       tweetPhone.add(TweenMax.to($('#phone-project-svg  path'), 1, {strokeDashoffset: 0, ease:Linear.easeNone}));
 
-  var scene0 = new ScrollMagic.Scene({triggerElement: '#tnd-single-project-spacer', duration: 300, tweenChanges: true})
+  var scene0 = new ScrollMagic.Scene({triggerElement: '#animation-start', duration: 300, tweenChanges: true})
       .setTween(tweetPhone)
       .addTo(controller);
 /*----- END Animation SMARTPHONE */
@@ -63,7 +65,7 @@ $(document).ready(function() {
   var tweenArrow = new TimelineMax();
       tweenArrow.add(TweenMax.to($('#arrow-svg path'), 1, {strokeDashoffset: 0, ease:Linear.easeNone}));
 
-  var scene0 = new ScrollMagic.Scene({triggerElement: '#tnd-single-project-spacer', duration: 300, tweenChanges: true})
+  var scene0 = new ScrollMagic.Scene({triggerElement: '#animation-start', duration: 300, tweenChanges: true})
       .setTween(tweenArrow)
       .addTo(controller);
 /*----- END Animation SMARTPHONE */
