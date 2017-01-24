6d7605777e33eaa4eedf7a6f7830888bc3dc5b4f
Sara <sarabertaina@hotmail.it>
2017-01-24 17:03:51 +0100

    Built jekyll-project from commit 485b8ea

--- customjs/projects.js
+++ customjs/projects.js
@@ -10,15 +10,25 @@ $(window).load(function() {
     $grid.isotope({ filter: filterValue });
     $('.projects-menu li').removeClass('active');
     $(this).addClass('active');
+    if($(window).width() <= 500){
+      $("#tnd-project-spacer .row-menu").css("height", "0px");
+      $(this).removeClass("active");
+      $(".fa-angle-down").hide();
+      $(".fa-angle-up").show();
+    }
   });
 
   $('.mobile-filter').on( 'click', function() {
     if($(this).hasClass("active")){
       $("#tnd-project-spacer .row-menu").css("height", "0px");
       $(this).removeClass("active");
+      $(".fa-angle-down").hide();
+      $(".fa-angle-up").show();
 
     }else{
       $("#tnd-project-spacer .row-menu").css("height", "auto");
+      $(".fa-angle-down").show();
+      $(".fa-angle-up").hide();
       $(this).addClass("active");
     }
   });
