9f280ac86e0728d066bae9969b8f0ceb03de3350
Sara <sarabertaina@hotmail.it>
2017-01-25 10:28:26 +0100

    Built jekyll-project from commit 96903de

--- customjs/projects.js
+++ customjs/projects.js
@@ -22,14 +22,12 @@ $(window).load(function() {
   $('.mobile-filter').on( 'click', function() {
     if($(this).hasClass("active")){
       $("#tnd-project-spacer .row-menu").css("height", "0px");
-      $("html").css("overflow-y", "auto");
       $(this).removeClass("active");
       $(".fa-angle-down").hide();
       $(".fa-angle-up").show();
 
     }else{
       $("#tnd-project-spacer .row-menu").css("height", "auto");
-      $("html").css("overflow-y", "hidden");
       $(".fa-angle-down").show();
       $(".fa-angle-up").hide();
       $(this).addClass("active");
