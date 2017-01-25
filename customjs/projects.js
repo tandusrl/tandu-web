67e7f558c43898a7a73ac508160682d645acfb96
Sara <sarabertaina@hotmail.it>
2017-01-25 08:56:47 +0100

    Built jekyll-project from commit a969726

--- customjs/projects.js
+++ customjs/projects.js
@@ -15,18 +15,21 @@ $(window).load(function() {
       $(this).removeClass("active");
       $(".fa-angle-down").hide();
       $(".fa-angle-up").show();
+      $("html").css("overflow-y", "auto");
     }
   });
 
   $('.mobile-filter').on( 'click', function() {
     if($(this).hasClass("active")){
       $("#tnd-project-spacer .row-menu").css("height", "0px");
+      $("html").css("overflow-y", "auto");
       $(this).removeClass("active");
       $(".fa-angle-down").hide();
       $(".fa-angle-up").show();
 
     }else{
       $("#tnd-project-spacer .row-menu").css("height", "auto");
+      $("html").css("overflow-y", "hidden");
       $(".fa-angle-down").show();
       $(".fa-angle-up").hide();
       $(this).addClass("active");
